802e0f88a79aa25fa8215c7d312b9e3fd0c57b90
Soyoda Meenmoy <meenmoyafrin@gmail.com>
2026-04-20 23:59:52 +0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,8 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 app.use(cors());
 app.use(express.json());
